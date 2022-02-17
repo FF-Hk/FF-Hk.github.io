2979a7cdf70833a555d131d4cb6051311a0c50b7
bread-ly <56876433+bread-ly@users.noreply.github.com>
2022-02-17 16:22:46 +0100

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -238,7 +238,7 @@ function Inv() {
             if (cam.gettext() != null && cam.gettext() != NaN && !scanneddata.includes(cam.getid())) {
                 scanneddata.push(cam.getid());
             }
-            showtext.innerHTML = "Zur Inventur weitere Barcodes Scannen!";
+            showtext.innerHTML = "Raum: " + room + "\n Zur Inventur weitere Barcodes Scannen!";
             Inventoryresult();
         } else if (init == false) {
             if (cam.gettext() != null && cam.gettext() != NaN && !scanneddata.includes(cam.getid())) {
