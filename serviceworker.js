c55282720235dd843eb593e7a8d4465bb0b1960c
bread-ly <lukas.adelmann@gmail.com>
2022-03-25 12:41:33 +0100

    service worker update

--- serviceworker.js
+++ serviceworker.js
@@ -1,2 +1,21 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('v1').then(function(cache) {
+        return cache.addAll([
+          '/navbar.js',
+          '/scanner.js',
+          '/show.js',
+          '/showdata.js',
+          '/style.css',
+          '/classes/camera.js',
+          '/classes/init.js',
+          '/classes/pdf.js'
+        ]);
+      })
+    );
+  });
 self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+    );
 });
