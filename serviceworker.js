be5372182711996a5d043bcc2f490d0dcdaa8096
bread-ly <56876433+bread-ly@users.noreply.github.com>
2022-03-28 09:38:26 +0200

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,25 +1,3 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-          caches
-            .open('v1')
-            .then(cache => {
-              cache.addAll([
-                'navbar.js',
-                'scanner.js',
-                'show.js',
-                '/classes/camera.js',
-                '/classes/init.js',
-                '/classes/pdf.js'
-              ]);
-            })
-            .then(() => self.skipWaiting())
-        );
-});
 
 self.addEventListener('fetch', event => {
-    console.log("fetch");
-    event.respondWith(
-        caches.match(event.request)
-    );
-    console.log("fetched");
 });
